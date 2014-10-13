99def9cda319de75d2170c9de2aa200ff3f964d0
Sean O'Connor <sean@bit.ly>
2014-10-12 22:26:44 -0400

    Improved project infrastructure and added key press handling.

--- app/router.js
+++ app/router.js
@@ -1,5 +1,6 @@
 var Backbone = require('backbone');
 var $ = require('jquery');
+var _ = require('lodash');
 
 Backbone.$ = $;
 
@@ -14,6 +15,7 @@ module.exports = Backbone.Router.extend({
 
     initialize: function(options) {
         this.appView = options.appView;
+        $(document).keydown(_.bind(this.handleKeyDown, this))
     },
 
     title: function() {
@@ -25,5 +27,36 @@ module.exports = Backbone.Router.extend({
 
     defaultRoute: function(actions) {
         this.navigate("title");
+    },
+
+    handleKeyDown: function(event) {
+        console.log("Keypress!");
+        console.log(event.which);
+        switch(event.which) {
+            case 34:  // pageDn
+            case 39:  // right
+            case 40:  // down
+            case 32:  // space
+                this.next();
+                break;
+            case 33:  // pageUp
+            case 38:  // up
+            case 37:  // left
+                this.prev();
+                break;
+            case 27:  // esc
+                this.navigate("title");
+                break;
+            default:
+                return;
+        }
+    },
+
+    next: function() {
+        console.log('next!');
+    },
+
+    prev: function() {
+        console.log('prev!');
     }
 });
